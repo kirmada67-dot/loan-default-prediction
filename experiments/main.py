065066b712c771e71ebb133d3401f5301c580f47
kirmada67-dot <kirmada67@gmail.com>
2026-05-05 13:57:24 +0530

    updating

--- experiments/main.py
+++ experiments/main.py
@@ -3,5 +3,6 @@ import pandas as pd
 
 
 train = pd.read_csv("../data/train.csv")
+test = pd.read_csv("../data/test.csv")
+print(test.isna().sum())
 print(train.isna().sum())
-print(train["Loan_Status"].value_counts())
