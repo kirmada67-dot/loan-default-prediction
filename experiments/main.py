6d94da90054127c269a3ce37f3a5c34898d33c39
kirmada67-dot <kirmada67@gmail.com>
2026-05-07 19:46:57 +0530

    Data cleaning

--- experiments/main.py
+++ experiments/main.py
@@ -5,14 +5,26 @@ import pandas as pd
 train = pd.read_csv("../data/train.csv")
 test = pd.read_csv("../data/test.csv")
 
+#Cleaning
+train["Gender"].fillna(train["Gender"].mode()[0], inplace=True)
+train["Married"].fillna(train["Married"].mode()[0], inplace=True)
+train["Dependents"].fillna(train["Dependents"].mode()[0], inplace=True)
+train["Self_Employed"].fillna(train["Self_Employed"].mode()[0], inplace=True)
+
+test["Gender"].fillna(train["Gender"].mode()[0], inplace=True)
+test["Married"].fillna(train["Married"].mode()[0], inplace=True)
+test["Dependents"].fillna(train["Dependents"].mode()[0], inplace=True)
+test["Self_Employed"].fillna(train["Self_Employed"].mode()[0], inplace=True)
+
+train["LoanAmount"].fillna(train["LoanAmount"].median(), inplace=True)
+train["Loan_Amount_Term"].fillna(train["Loan_Amount_Term"].mode()[0], inplace=True)
+train["Credit_History"].fillna(train["Credit_History"].mode()[0], inplace=True)
+
+test["LoanAmount"].fillna(train["LoanAmount"].median(), inplace=True)
+test["Loan_Amount_Term"].fillna(train["Loan_Amount_Term"].mode()[0], inplace=True)
+test["Credit_History"].fillna(train["Credit_History"].mode()[0], inplace=True)
 
 
 
-
-
-
-
-
-
-print(test.isna().sum())
-print(train.isna().sum())
+print(train)
+print(test.head())
