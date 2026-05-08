86c756b738c3e4bb80179c325a468223773f9e52
kirmada67-dot <kirmada67@gmail.com>
2026-05-08 12:53:40 +0530

    updated mapping

--- experiments/main.py
+++ experiments/main.py
@@ -25,6 +25,27 @@ test["Loan_Amount_Term"].fillna(train["Loan_Amount_Term"].mode()[0], inplace=Tru
 test["Credit_History"].fillna(train["Credit_History"].mode()[0], inplace=True)
 
 
+#Mapping
+train["Gender"] = train["Gender"].map({"Male": 1, "Female": 0})
+train["Married"] = train["Married"].map({"Yes": 1, "No": 0})
+train["Education"] = train["Education"].map({"Graduate": 1, "Not Graduate": 0})
+train["Self_Employed"] = train["Self_Employed"].map({"Yes": 1, "No": 0})
+train["Loan_Status"] = train["Loan_Status"].map({"Y": 1, "N": 0})
 
-print(train)
-print(test.head())
+test["Gender"] = test["Gender"].map({"Male": 1, "Female": 0})
+test["Married"] = test["Married"].map({"Yes": 1, "No": 0})
+test["Education"] = test["Education"].map({"Graduate": 1, "Not Graduate": 0})
+test["Self_Employed"] = test["Self_Employed"].map({"Yes": 1, "No": 0})
+
+train["Dependents"] = train["Dependents"].replace("3+", 3)
+test["Dependents"] = test["Dependents"].replace("3+", 3)
+
+train["Dependents"] = train["Dependents"].astype(int)
+test["Dependents"] = test["Dependents"].astype(int)
+
+train = pd.get_dummies(train, columns=["Property_Area"], drop_first=True)
+test = pd.get_dummies(test, columns=["Property_Area"], drop_first=True)
+
+train.drop("Loan_ID", axis=1, inplace=True)
+test.drop("Loan_ID", axis=1, inplace=True)
+
