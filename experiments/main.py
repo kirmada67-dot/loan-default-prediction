ed625e232a5952d6f305d48c92775375e83d1b61
kirmada67-dot <kirmada67@gmail.com>
2026-05-18 22:56:48 +0530

    baseline RF

--- experiments/main.py
+++ experiments/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+#from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sbn
@@ -60,7 +61,7 @@ y = train["Loan_Status"]
 
 x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=42, stratify=y)
 
-model = LogisticRegression(max_iter=10000)
+model = RandomForestClassifier(random_state=42)
 model.fit(x_train, y_train)
 
 pred = model.predict(x_val)
