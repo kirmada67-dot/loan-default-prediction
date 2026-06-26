9cf31eaaa041820056b94d22a19cb09605b223d2
kirmada67-dot <kirmada67@gmail.com>
2026-06-26 16:09:19 +0530

    preprocessing

---
+++ src/preprocess.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+
+train = pd.read_csv("../data/train.csv")
+test = pd.read_csv("../data/test.csv")
+
+#Cleaning
+train["Gender"].fillna(train["Gender"].mode()[0], inplace=True)
+train["Married"].fillna(train["Married"].mode()[0], inplace=True)
+train["Dependents"].fillna(train["Dependents"].mode()[0], inplace=True)
+train["Self_Employed"].fillna(train["Self_Employed"].mode()[0], inplace=True)
+
+test["Gender"].fillna(train["Gender"].mode()[0], inplace=True)
+test["Married"].fillna(train["Married"].mode()[0], inplace=True)
+test["Dependents"].fillna(train["Dependents"].mode()[0], inplace=True)
+test["Self_Employed"].fillna(train["Self_Employed"].mode()[0], inplace=True)
+
+train["LoanAmount"].fillna(train["LoanAmount"].median(), inplace=True)
+train["Loan_Amount_Term"].fillna(train["Loan_Amount_Term"].mode()[0], inplace=True)
+train["Credit_History"].fillna(train["Credit_History"].mode()[0], inplace=True)
+
+test["LoanAmount"].fillna(train["LoanAmount"].median(), inplace=True)
+test["Loan_Amount_Term"].fillna(train["Loan_Amount_Term"].mode()[0], inplace=True)
+test["Credit_History"].fillna(train["Credit_History"].mode()[0], inplace=True)
+
+
+#Mapping
+train["Gender"] = train["Gender"].map({"Male": 1, "Female": 0})
+train["Married"] = train["Married"].map({"Yes": 1, "No": 0})
+train["Education"] = train["Education"].map({"Graduate": 1, "Not Graduate": 0})
+train["Self_Employed"] = train["Self_Employed"].map({"Yes": 1, "No": 0})
+train["Loan_Status"] = train["Loan_Status"].map({"Y": 1, "N": 0})
+
+test["Gender"] = test["Gender"].map({"Male": 1, "Female": 0})
+test["Married"] = test["Married"].map({"Yes": 1, "No": 0})
+test["Education"] = test["Education"].map({"Graduate": 1, "Not Graduate": 0})
+test["Self_Employed"] = test["Self_Employed"].map({"Yes": 1, "No": 0})
+
+train["Dependents"] = train["Dependents"].replace("3+", 3)
+test["Dependents"] = test["Dependents"].replace("3+", 3)
+
+train["Dependents"] = train["Dependents"].astype(int)
+test["Dependents"] = test["Dependents"].astype(int)
+
+train = pd.get_dummies(train, columns=["Property_Area"], drop_first=True)
+test = pd.get_dummies(test, columns=["Property_Area"], drop_first=True)
+
+train.drop("Loan_ID", axis=1, inplace=True)
+test.drop("Loan_ID", axis=1, inplace=True)
+
+
